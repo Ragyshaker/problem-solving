647538cb0f5ce7096c941f95851b60115c8196b5
ragy shaker <ragyshaker1110@gmail.com>
2024-02-26 15:57:19 +0200

    Update A. IQ Test.cpp

--- A. IQ Test/A. IQ Test.cpp
+++ A. IQ Test/A. IQ Test.cpp
@@ -1,60 +1,37 @@
 
 //problem link : https://codeforces.com/contest/287/problem/A 
 
-#include<iostream>
-#include<cmath>
-#define endl "\n"
-#define ll long long
-#define FAST ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-using namespace std;
-int main() {
-    FAST;
-    int n, m, k, value, maxR1 = 0, maxP1 = 0, minP2 = 5000, r1;
-    double  A, B, ratio, r2;
-    cin >> n >> m >> k;
+#include<iostream>  
+#include<cmath>    
+#define endl "\n"   
+#define ll long long 
+#define FAST ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0); 
+using namespace std; 
 
-    // Loop to input possible values of r1 and to find the max value
-    for (int i = 0; i < n; i++)
-    {
-        cin >> value;
-        if (value > maxR1)
-        {
-            maxR1 = value;
+int main() { 
+    FAST; // Macro for fast input-output operations
+    char arr[4][4]; // Declare a 2D character array of size 4x4
+
+    // Loop to input values into the array
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            cin >> arr[i][j];
         }
     }
-    r1 = maxR1;
-    maxR1 *= maxR1;
-    // Loop to input possible values of p1 and to find the max value
-    for (int i = 0; i < m; i++)
-    {
-        cin >> value;
-        if (value > maxP1)
-        {
-            maxP1 = value;
+
+    // Loop to iterate through subgrids
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            // Calculate the sum of the elements in the current subgrid
+            int sum = arr[i][j] + arr[i + 1][j] + arr[i][j + 1] + arr[i + 1][j + 1];
+            // If the sum is not equal to 162 (ASCII value of '#' + '#' + '.' + '.'), print "YES" and return
+            if (sum != 162)
+            {
+                cout << "YES";
+                return 0;
+            }
         }
     }
-    // Loop to input possible values of p2 and to find the min value
-    for (int i = 0; i < k; i++)
-    {
-        cin >> value;
-        if (value <= minP2)
-        {
-            minP2 = value;
-        }
-    }
-    cin >> A >> B;
-    ratio = A / B;
-    r2 = sqrt((maxP1 * maxR1) / ratio);
-    if (r2 < r1) {
-        cout << r2;
-    }
-    else
-    {
-        while (r2 >= r1) {
-            ratio++;
-            r2 = sqrt((maxP1 * maxR1) / ratio);  
-        }
-        cout << r2;
-    }
+    cout << "NO";
     return 0;
 }
